702013a848f6f767b3f1c1f49a59a825caab75fe
yunseopkim2 <dbstjqdlwksj@gmail.com>
2022-04-12 10:37:37 +0900

    new baranch

--- app/routes/basic.route.js
+++ app/routes/basic.route.js
@@ -4,3 +4,4 @@ module.exports = x => x.app.post(`${x.url}/bmi`, getBmi);
 module.exports = x => x.app.post(`${x.url}/calc`, getCalc);
 
 module.exports = x => x.app.post(`${x.url}/grade`, getGrade) ;
+//module.exports = x => x.app.post(`${x.url}/grade`, getGrade) ;
